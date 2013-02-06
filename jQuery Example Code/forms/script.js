dbe6f7aead980dddb5167c2c343cc8b39ac2588c
Jonathan Paul <jonathan.paul@ixxus.com>
2013-02-06 16:53:54 +0000

    Initial upload of files

---
+++ jQuery Example Code/forms/script.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+  $('#signup form').validate({
+    rules: {
+      name: {
+        required: true,
+      },
+      email: {
+        required: true,
+        email: true
+      },
+      website: {
+        url: true
+      },
+	  gender: {
+        required: true
+      },
+      password: {
+        minlength: 6,
+        required: true
+      },
+      passconf: {
+        equalTo: "#password"
+      }
+    },
+    success: function(label) {
+      label.text('OK!').addClass('valid');
+    }
+  });
+});
+
